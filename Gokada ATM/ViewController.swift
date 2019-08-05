ba47daa330b2c4289ac6702bdc45ca1cc582f276
Isaac Iniongun <iniongunisaac@gmail.com>
2019-08-05 19:40:15 +0100

    Initial Commit

---
+++ Gokada ATM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Gokada ATM
+//
+//  Created by Isaac Iniongun on 05/08/2019.
+//  Copyright © 2019 Ing Groups. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Gokada ATMTests/Gokada_ATMTests.swift
@@ -0,0 +1,34 @@
+//
+//  Gokada_ATMTests.swift
+//  Gokada ATMTests
+//
+//  Created by Isaac Iniongun on 05/08/2019.
+//  Copyright © 2019 Ing Groups. All rights reserved.
+//
+
+import XCTest
+@testable import Gokada_ATM
+
+class Gokada_ATMTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
